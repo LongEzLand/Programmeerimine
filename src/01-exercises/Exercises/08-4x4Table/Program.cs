33322675602e91df6dbe8c300b891bf4d8767a66
greteliis <greteliis.pint@gmail.com>
2017-10-23 15:55:50 +0300

    Add 4x4Table

---
+++ src/01-exercises/Exercises/08-4x4Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_4x4Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.Write(i + " | ");
+                for (int j = 1; j <= 4; j++)
+                {
+                    if (i > 0) Console.Write(i * j + " | ");
+                    else Console.Write(j + " | ");
+                }
+                Console.Write("\n");
+                Console.Write("----------------");
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
