04305e7c30974ad8119353fabf5bd44ab0aeb801
greteliis <greteliis.pint@gmail.com>
2017-10-23 14:24:16 +0300

    Add Parrot exercise - Variable and input

---
+++ src/01-exercises/Exercises/03-papagoi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_papagoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen papagoi programm. Ütle mulle midagi ja ma kordan selle sulle tagasi...");
+            Console.Write(Environment.NewLine);
+
+
+
+            Console.Write("Sina ütled: ");
+            String inputWord = Console.ReadLine();
+
+            Console.WriteLine("Mina ütlen: " + inputWord);
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Press any key to continue..");
+            Console.ReadLine();
+
+
+        }
+    }
+}
