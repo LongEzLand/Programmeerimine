a167f911ea982e7668ceb8eb40530cf6c421bdb8
greteliis <greteliis.pint@gmail.com>
2017-10-23 15:43:59 +0300

    Add Excuses Print App using Foreach

---
+++ src/01-exercises/Exercises/07-ExcusesUsingForeach/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ExcusesUsingForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                string[] excuses = new[] { "Koer sõi kodutöö ära", "Ema ei luba arvutit kasutada", "Paha oli olla" };
+            int i = 0;
+                foreach(string element in excuses)
+                {
+                    Console.WriteLine($"{i + 1}. {element}");
+                i++;
+
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+
