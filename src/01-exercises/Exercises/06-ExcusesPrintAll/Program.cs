9730c978fd5baa35872425d535008036831e63ef
greteliis <greteliis.pint@gmail.com>
2017-10-23 15:35:01 +0300

    Add Excuses Print All App (For Loop)

---
+++ src/01-exercises/Exercises/06-ExcusesPrintAll/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ExcusesPrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] excuses = new[] { "Koer sõi kodutöö ära", "Ema ei luba arvutit kasutada", "Paha oli olla" };
+
+            for (int i = 0; i < excuses.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {excuses[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
