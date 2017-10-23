20c15b4e579720e8210a3a4b28956ce4370db5a4
greteliis <greteliis.pint@gmail.com>
2017-10-23 15:27:06 +0300

    Add Magic Number Guessing Game

---
+++ src/01-exercises/Exercises/05-GuessAMagicNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_GuessAMagicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ma valin välja ühe suvalise numbri [1-100]. Proovi see ära arvata :)");
+
+            Random rnd = new Random();
+            int randomNr = rnd.Next(1, 101);
+
+            bool succesfulGuess = false;
+            
+            while (!succesfulGuess)
+            {
+                Console.Write("Sina ütled: ");
+                int userNr = int.Parse(Console.ReadLine());
+
+                if (userNr > randomNr)
+                {
+                    Console.WriteLine("Minu number on väiksem");
+                }
+                else if (userNr < randomNr)
+                {
+                Console.WriteLine("Minu number on suurem");
+                } else
+                {
+                    Console.WriteLine("Arvasid ära. Tubli!");
+                    succesfulGuess = true;
+                }
+            }
+            
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+        }
+    }
+}
