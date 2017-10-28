21d58fd3cc5f488c082ab01ca878454fd6bd6be8
greteliis <greteliis.pint@gmail.com>
2017-10-28 11:12:48 +0300

    Add graded assignment

--- src/02-graded-assignment/GradedAssignment/GradedAssignment/Program.cs
+++ src/02-graded-assignment/GradedAssignment/GradedAssignment/Program.cs
@@ -10,6 +10,17 @@ namespace GradedAssignment
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is a discount calculator and please insert a sum ($): ");
+
+            double userInput = double.Parse(Console.ReadLine());
+
+            if (userInput > 50)
+            {
+                double discountsum = userInput * 0.95;
+                Console.WriteLine("Your discount percent is 5% and have to pay: " + discountsum);
+            }
+            Console.WriteLine("Enter any key to continue...");
+            Console.ReadLine();
         }
     }
 }
