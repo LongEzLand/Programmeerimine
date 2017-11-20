759f030956cc4c3caecf8fba9b2fc7816615dd32
greteliis <greteliis.pint@gmail.com>
2017-11-20 17:39:07 +0200

    Adding Restaurant Receipt

---
+++ src/06-graded-assignment-restaurant-receipt-oop/RestaurantReceipt/OopRestaurantReceipt/Receipt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace RestaurantReceipt
+{
+    public class Receipt
+    {
+        private string name;
+        private string address;
+        private Tab tab;
+
+        public Receipt(string name, string address, Tab tab)
+        {
+            this.name = name;
+            this.address = address;
+            this.tab = tab;            
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            double subtotal = 0;
+            sb.AppendLine(name);
+            sb.AppendLine(address);
+            sb.AppendLine("----------------------------------------");
+            foreach (var item in tab.Items)
+            {
+                sb.AppendLine($"Price of food item: {item}");
+                subtotal += item;
+            }
+            sb.AppendLine("----------------------------------------");
+            sb.AppendLine($"Subtotal: €{subtotal}");
+            sb.AppendLine($"15,00% Gratuity: €{subtotal * 0.15:F2}");
+            sb.AppendLine($"Total: €{subtotal - (subtotal * 0.15):F2}");
+
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/06-graded-assignment-restaurant-receipt-oop/RestaurantReceipt/OopRestaurantReceipt/Tab.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantReceipt
+{
+    public class Tab
+    {
+        public Tab()
+        {
+        }
+
+        public List<double> Items = new List<double>();
+
+        public void Add(double v)
+        {
+            Items.Add(v);
+
+        }
+    }
+}
